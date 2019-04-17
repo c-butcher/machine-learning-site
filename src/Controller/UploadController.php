43404e3c3dce2f517efe3972ecbab14f4339476f
Chris Butcher <c.butcher@hotmail.com>
2019-04-17 12:17:20 -0700

    [Issue #9] - Upload action is now creating the DataColumns and setting more information on DataSet.

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\DataSet;
+use App\Entity\DataColumn;
 use App\Form\DataSetUploadType;
+use App\Service\DataReader;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +26,8 @@ class UploadController extends AbstractController
      * @param Session $session
      *
      * @return Response
+     *
+     * @throws \Exception
      */
     public function uploadAction(Request $request, Session $session)
     {
@@ -33,22 +37,47 @@ class UploadController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
+            // Move the spreadsheet to the uploads directory,
+            // or return to the uploads page when it fails.
             $uploadedFile = $dataset->getUploadedFile();
-            if ($this->saveSpreadsheet($uploadedFile)) {
-
-                $dataset->setFilename($uploadedFile->getClientOriginalName());
-
-                $manager = $this->getDoctrine()->getManager();
-                $manager->persist($dataset);
-                $manager->flush();
-
-                return $this->redirectToRoute('describe_dataset', [
-                    'dataset' => $dataset->getId(),
-                ]);
-
-            } else {
+            if (!$this->saveSpreadsheet($uploadedFile)) {
                 $session->getFlashBag()->add( 'error', 'Unable to upload the spreadsheet.');
+
+                return $this->render('upload/spreadsheet.html.twig', [
+                    'form' => $form->createView()
+                ]);
             }
+
+            // Set the filename of the spreadsheet.
+            $dataset->setFilename($uploadedFile->getClientOriginalName());
+
+            // Read the spreadsheet and get all of the column types.
+            $reader  = new DataReader($dataset, $this->getParameter('spreadsheets_directory'));
+
+            // We are fetching all the columns from the spreadsheet,
+            // and populating our DataColumn objects.
+            $columns = $reader->getColumnTypes();
+            foreach ($columns as $name => $type) {
+                $column = new DataColumn();
+                $column->setName($name);
+                $column->setType($type);
+                $column->setRequired(true);
+
+                $dataset->addColumn($column);
+            }
+
+            // Set the number of columns and rows in the spreadsheet.
+            $dataset->setNumColumns(count($columns));
+            $dataset->setNumRows($reader->getNumRows());
+
+            // Save the dataset to the database.
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($dataset);
+            $manager->flush();
+
+            return $this->redirectToRoute('describe_dataset', [
+                'dataset' => $dataset->getId(),
+            ]);
         }
 
         return $this->render('upload/spreadsheet.html.twig', [
@@ -57,6 +86,9 @@ class UploadController extends AbstractController
     }
 
     /**
+     * Moves are spreadsheet from the temporary upload directory to our final
+     * spreadsheets directory.
+     *
      * @param UploadedFile $file
      *
      * @return bool
